837293c13620ddbab85df722b1baba706357c31f
Alex Rehberg <hi@alexrehberg.com>
2014-02-01 18:28:35 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CreatorhuddleCom::Application.config.secret_key_base = 'bcfdadc66dedd4fb6711b4a9aae4681ed6746ab946536c748c213e16d6d90454837c083d72b51ab0f76294e37ffee1ca1394b4278dcff5ce2ff4ac6a76b4291c'
