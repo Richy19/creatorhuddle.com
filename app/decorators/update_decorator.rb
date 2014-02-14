64b77444ec61652f697ab9d30e557bc3041cb09d
Alex Rehberg <hi@alexrehberg.com>
2014-02-14 15:35:31 -0800

    Enable markdown for updates

--- app/decorators/update_decorator.rb
+++ app/decorators/update_decorator.rb
@@ -2,4 +2,13 @@ class UpdateDecorator < Draper::Decorator
   delegate_all
   decorates_association :user
   decorates_association :comments
+
+  def content
+    if object.content.blank?
+      ''
+    else
+      markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, space_after_headers: true)
+      markdown.render(object.content).html_safe
+    end
+  end
 end
